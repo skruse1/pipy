697c091b08b71207d0d0e3189a1063905c1c8e50
skruse1 <46829681+skruse1@users.noreply.github.com>
2020-04-16 08:28:41 -0500

    Update signal.py

--- signal.py
+++ signal.py
@@ -1,40 +1,79 @@
 #!/usr/bin/env python
-
-import signal
-
 import skywriter
+import signal
+import RPi.GPIO as GPIO
+import time
 
-
+channelleft = 21
+channelright = 20
+blinkleft = 12
+blinkright = 16
+GPIO.setmode(GPIO.BCM)
 some_value = 5000
+GPIO.setup(channelleft, GPIO.OUT)
+GPIO.setup(channelright, GPIO.OUT)
+GPIO.setup(blinkleft, GPIO.OUT)
+GPIO.setup(blinkright, GPIO.OUT)
 
-@skywriter.move()
-def move(x, y, z):
-    print( x, y, z )
+GPIO.output(16,1)
+time.sleep(1)
+
+#@skywriter.move()
+#def move(x, y, z):
+#  print( x, y, z )
+def motor_on(pin):
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+   
+def motor_off(pin):
+    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+
 
 @skywriter.flick()
 def flick(start,finish):
-    print('Got a flick!', start, finish)
-
+  if start == 'east':
+      print('Turn Left')
+      motor_on(channelleft)
+  elif start == 'west':
+      print('Turn Right')
+      motor_on(channelright)
+  elif start == 'south':
+      print('Off')
+      motor_off(channelleft)
+      motor_off(channelright)
+  elif start == 'north':
+      print('Off')
+      motor_off(channelleft)
+      motor_off(channelright)
+     
 @skywriter.airwheel()
 def spinny(delta):
-    global some_value
-    some_value += delta
-    if some_value < 0:
-        some_value = 0
-    if some_value > 10000:
-        some_value = 10000
-    print('Airwheel:', some_value/100)
+  global some_value
+  some_value += delta
+ 
+ 
+  if some_value > 10000:
+    some_value = 10000
+  print('Off')
+  motor_off(channelleft)
+  motor_off(channelright)
+
 
 @skywriter.double_tap()
 def doubletap(position):
-    print('Double tap!', position)
-
+  print('Off', position)
+  motor_off(channelleft)
+  motor_off(channelright)
+ 
 @skywriter.tap()
 def tap(position):
-    print('Tap!', position)
-
+  print('Off', position)
+  motor_off(channelleft)
+  motor_off(channelright)
+ 
 @skywriter.touch()
 def touch(position):
-    print('Touch!', position)
-
+  print('Off', position)
+  motor_off(channelleft)
+  motor_off(channelright)
+ 
 signal.pause()
